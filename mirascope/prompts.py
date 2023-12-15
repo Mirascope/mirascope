b46998ee5ecb37753c3fd4d6517fa67865fcf531
William Bakst <will@sotai.ai>
2023-12-14 20:36:37 -0800

    feat:  for single message with unit tests.

--- mirascope/prompts.py
+++ mirascope/prompts.py
@@ -1,21 +1,48 @@
 """A class for better prompting."""
 
 from __future__ import annotations
-from pydantic import BaseModel
+
 import pickle
+import re
+from string import Formatter
+from textwrap import dedent
+
+from pydantic import BaseModel
 
 
 class MirascopePrompt(BaseModel):
     """A Pydantic model for prompts."""
 
     @classmethod
-    def template(cls):
-        """Returns the docstring template."""
-        return cls.__doc__
+    def template(cls) -> str:
+        """Custom parsing functionality for docstring prompt.
 
-    def __str__(self):
-        """Returns the formatted prompt."""
-        return self.__doc__.strip().format(**self.__dict__)
+        This function is the first step in formatting the prompt template docstring.
+        For the default `MirascopePrompt`, this function dedents the docstring and
+        replaces all repeated sequences of newlines with one fewer newline character.
+        This enables writing blocks of text instead of really long single lines. To
+        include any number of newline characters, simply include one extra.
+
+        Raises:
+            ValueError: If the docstring is empty.
+        """
+        if cls.__doc__ is None:
+            raise ValueError("`MirascopePrompt` must have a prompt template docstring.")
+
+        template = dedent(cls.__doc__).strip("\n")
+        template = re.sub(
+            "(\n+)", lambda x: x.group(0)[:-1] if len(x.group(0)) > 1 else " ", template
+        )
+
+        return template
+
+    def __str__(self) -> str:
+        """Returns the docstring prompt template formatted with all template variables."""
+        template = self.template()
+        template_vars = [
+            var for _, var, _, _ in Formatter().parse(template) if var is not None
+        ]
+        return template.format(**{var: getattr(self, var) for var in template_vars})
 
     def save(self, filepath: str):
         """Saves the prompt to the given filepath."""
--- tests/test_prompts.py
+++ tests/test_prompts.py
@@ -2,12 +2,45 @@
 from mirascope.prompts import MirascopePrompt
 
 
-def test_mirascope_prompt_template():
+class FooPrompt(MirascopePrompt):
+    """
+    This is a test prompt about {foobar}.
+    This should be on the same line in the template.
+
+        This should be indented on a new line in the template.
+    """
+
+    foo: str
+    bar: str
+
+    @property
+    def foobar(self) -> str:
+        """Returns `foo` and `bar` concatenated."""
+        return self.foo + self.bar
+
+
+def test_template():
     """Test that `MirascopePrompt` initializes properly."""
+    print(FooPrompt.template())
+    assert (
+        FooPrompt.template() == "This is a test prompt about {foobar}. "
+        "This should be on the same line in the template."
+        "\n    This should be indented on a new line in the template."
+    )
 
-    class TestPrompt(MirascopePrompt):
-        """{foo}"""
 
-        foo: str
+def test_str():
+    """Test that the `__str__` method properly formats the template."""
+    assert (
+        str(FooPrompt(foo="foo", bar="bar")) == "This is a test prompt about foobar. "
+        "This should be on the same line in the template."
+        "\n    This should be indented on a new line in the template."
+    )
 
-    assert TestPrompt.template() == "{foo}"
+
+def test_save_and_load(tmpdir):
+    """Test that `MirascopePrompt` can be saved and loaded."""
+    prompt = FooPrompt(foo="foo", bar="bar")
+    filepath = f"{tmpdir}/test_prompt.pkl"
+    prompt.save(filepath)
+    assert FooPrompt.load(filepath) == prompt
