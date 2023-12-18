50572474ae9df95d33d91b2b20bd27785f8c8580
Brendan <brendan.kao@sotai.ai>
2023-12-18 14:39:11 -0800

    redid imports

--- examples/project-with-cli/mirascope/versions/my_prompt/0001_my_prompt.py
+++ examples/project-with-cli/mirascope/versions/my_prompt/0001_my_prompt.py
@@ -1,12 +1,12 @@
 """Basic Prompt + LLM Example."""
-from mirascope.prompt import MirascopePromptTemplate
+from mirascope.prompts import MirascopePrompt
 
 
 prev_revision_id = "None"
 revision_id = "0001"
 
 
-class MyPrompt(MirascopePromptTemplate):
+class MyPrompt(MirascopePrompt):
     """This is a prompt 0001. It has a {noun} and a {verb}."""
 
     noun: str
--- examples/project-with-cli/prompts/my_prompt.py
+++ examples/project-with-cli/prompts/my_prompt.py
@@ -1,9 +1,8 @@
 """Basic Prompt + LLM Example."""
-from mirascope.prompt import MirascopePromptTemplate
+from mirascope.prompts import MirascopePrompt
 
 
-
-class MyPrompt(MirascopePromptTemplate):
+class MyPrompt(MirascopePrompt):
     """This is a prompt 0005. It has a {noun} and a {verb}."""
 
     noun: str
