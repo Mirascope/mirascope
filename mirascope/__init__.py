c9fa5992874e6eedec907d6fc59647d2fd73484e
William Bakst <will@sotai.ai>
2023-12-31 02:36:40 -0800

    chore: add __version__ to package init

--- mirascope/__init__.py
+++ mirascope/__init__.py
@@ -1,5 +1,8 @@
 """mirascope package."""
+import importlib.metadata
 
 from . import prompts
 from .chat.models import OpenAIChat
 from .prompts import Prompt
+
+__version__ = importlib.metadata.version("mirascope")
