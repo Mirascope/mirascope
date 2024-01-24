974b5bed38f9ef415ee313c37e4f0a533e3d94bd
Brian Park <brianpark@brians-air-2.mynetworksettings.com>
2024-01-24 17:42:52 -0500

    fix: resolved issues with code cleanliness

--- cookbook/basic_rag_example/pinecone_embeddings.py
+++ cookbook/basic_rag_example/pinecone_embeddings.py
@@ -17,45 +17,69 @@ os.environ["OPENAI_API_KEY"] = "YOUR_API_KEY"
 chat = OpenAIChat()
 embeddings_model = "text-embedding-ada-002"
 
-if not os.path.exists("cookbook/basic_rag_example/news_article_dataset.csv"):
-    url = "https://raw.githubusercontent.com/Dawit-1621/BBC-News-Classification/main/Data/BBC%20News%20Test.csv"
+
+def load_data(url) -> pd.DataFrame:
+    """Loads data from a url, and splits larger texts into smaller chunks."""
     df = pd.read_csv(url)
     split_articles = []
     encoder = tiktoken.encoding_for_model("gpt-3.5-turbo")
+
+    def split_text(text, tokens, max_tokens):
+        """Roughly splits a text into chunks of max_tokens size.
+
+        Note that tokens and characters do not have an exact correspondence, so in
+        certain edge cases a chunk may be slightly larger than max_tokens.
+        """
+        words = text.split()
+        num_splits = len(tokens) // max_tokens + 1
+        words_per_split = len(words) // num_splits
+        split_texts = [
+            " ".join(words[i : i + words_per_split])
+            for i in range(0, len(words), words_per_split + 1)
+        ]
+        return split_texts
 
     for i, row in df.iterrows():
         text = row["Text"]
         tokens = encoder.encode(text)
         if len(tokens) > 1000:
-            words = text.split()
-            num_splits = len(tokens) // 1000 + 1
-            words_per_split = len(words) // num_splits
-            split_texts = [
-                " ".join(words[j : j + words_per_split])
-                for j in range(0, len(words), words_per_split + 1)
-            ]
-            split_articles += split_texts
+            split_articles += split_text(text, tokens, 1000)
             df.drop(i, inplace=True)
 
+    # Long texts which were dropped from the dataframe are now readded.
     df = pd.concat(
         [df, pd.DataFrame(split_articles, columns=["Text"])], ignore_index=True
     )
+
+    return df
+
+
+def embed_data(df, batch_size):
+    """Embeds a dataframe of texts and stores it in a new column."""
     embeddings = []
     # ada 02 supports ~8000 tokens and 8 articles fit in the max batch size
-    for start_idx in range(0, len(df), 8):
-        end_idx = start_idx + 8
-        batch = df["Text"][start_idx:end_idx].tolist()
+    for start in range(0, len(df), batch_size):
+        end = start + batch_size
+        batch = df["Text"][start:end].tolist()
         embeddings_response = chat.client.embeddings.create(
             model=embeddings_model, input=batch
         )
         embeddings += [datum.embedding for datum in embeddings_response.data]
-
     df["embeddings"] = embeddings
-    df.to_csv(os.getcwd() + "/cookbook/basic_rag_example/news_article_dataset.csv")
+
+    return df
+
+
+filename = "news_article_dataset.csv"
+# Make reruns efficient by preprocessing data before saving to csv
+if not os.path.exists("cookbook/basic_rag_example/news_article_dataset.csv"):
+    url = "https://raw.githubusercontent.com/Dawit-1621/BBC-News-Classification/main/Data/BBC%20News%20Test.csv"
+    df = load_data(url)
+    df = embed_data(df, 8)  # ada 02 supports ~8000 tokens, our max_tokens is 1000
+    df.to_csv(f"{os.getcwd()}/cookbook/basic_rag_example/{filename}")
 else:
-    df = pd.read_csv(
-        os.getcwd() + "/cookbook/basic_rag_example/news_article_dataset.csv"
-    )
+    df = pd.read_csv(f"{os.getcwd()}/cookbook/basic_rag_example/{filename}")
+
 
 pc = Pinecone(api_key="YOUR_API_KEY")
 existing_indexes = pc.list_indexes().names()
@@ -73,13 +97,18 @@ if index_name not in existing_indexes:
 index = pc.Index(index_name)
 
 
-batch_size = len(df) // 2
-for batch_num, data in enumerate([df.iloc[:batch_size], df.iloc[batch_size:]]):
-    vectors = [
-        {"id": str(idx), "values": ast.literal_eval(row["embeddings"])}
-        for idx, row in data.iterrows()
-    ]
-    index.upsert(vectors=vectors, namespace=f"batch{batch_num+1}")
+batch_size = 300  # Pinecone can't handle too large of an upsert request
+for start in range(0, len(df), batch_size):
+    end = min(start + batch_size, len(df))
+    batch = df[start:end]
+    vectors = []
+    for i, row in batch.iterrows():
+        embeddings = row["embeddings"]
+        # Embeddings need to be parsed if they've been read from a csv
+        if isinstance(embeddings[0], str):
+            embeddings = ast.literal_eval(embeddings)
+        vectors.append({"id": str(i), "values": embeddings})
+    index.upsert(vectors=vectors, namespace="articles")
 
 
 class NewsPrompt(Prompt):
@@ -107,18 +136,15 @@ def summarize_news(query):
         .data[0]
         .embedding
     )
-    articles = []
-    for batch in ["batch1", "batch2"]:
-        query_response = index.query(namespace=batch, vector=query_embedding, top_k=2)
-        relevant_article_indices = [
-            int(article["id"]) for article in query_response["matches"]
-        ]
-        articles += relevant_article_indices
-    relevant_articles = df.loc[articles]["Text"].tolist()
+    query_response = index.query(namespace="articles", vector=query_embedding, top_k=4)
+    relevant_article_indices = [
+        int(article["id"]) for article in query_response["matches"]
+    ]
+    relevant_articles = df.loc[relevant_article_indices]["Text"].tolist()
     prompt = NewsPrompt(topic=query, articles=relevant_articles)
-    res = chat.create(prompt)
+    completion = chat.create(prompt)
     print(str(prompt))
-    print(res)
+    print(completion)
 
 
 summarize_news("teams and players going through trouble in soccer")
