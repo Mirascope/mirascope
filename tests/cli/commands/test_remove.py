dba4ae9a9cbedda874f871580ddbff6e8f2ba1ec
Brendan <brendan.kao@sotai.ai>
2024-02-20 13:20:19 -0800

    uncommented tests

--- tests/cli/commands/test_remove.py
+++ tests/cli/commands/test_remove.py
@@ -50,84 +50,84 @@ def test_remove(
     assert result.exit_code == 0
 
 
-# @patch("mirascope.cli.commands.remove.get_user_mirascope_settings")
-# @patch("mirascope.cli.commands.remove.get_prompt_versions")
-# @patch("mirascope.cli.commands.remove.find_prompt_path")
-# @patch("mirascope.cli.commands.remove.find_prompt_paths")
-# @patch("os.remove")
-# def test_remove_no_prompt_paths(
-#     mock_remove: MagicMock,
-#     mock_prompt_paths: MagicMock,
-#     mock_prompt_path: MagicMock,
-#     mock_prompt_versions: MagicMock,
-#     mock_settings: MagicMock,
-#     fixture_mirascope_user_settings: MirascopeSettings,
-#     fixture_prompt_versions: VersionTextFile,
-#     tmp_path: Path,
-# ):
-#     """Tests that `remove` removes a prompt"""
-#     version = "0001"
-#     version_path = tmp_path / f"{version}_test.py"
-#     mock_settings.return_value = fixture_mirascope_user_settings
-#     mock_prompt_versions.return_value = fixture_prompt_versions
-#     mock_prompt_paths.return_value = None
-#     mock_prompt_path.return_value = version_path
-#     prompt_file_name = "dan"
+@patch("mirascope.cli.commands.remove.get_user_mirascope_settings")
+@patch("mirascope.cli.commands.remove.get_prompt_versions")
+@patch("mirascope.cli.commands.remove.find_prompt_path")
+@patch("mirascope.cli.commands.remove.find_prompt_paths")
+@patch("os.remove")
+def test_remove_no_prompt_paths(
+    mock_remove: MagicMock,
+    mock_prompt_paths: MagicMock,
+    mock_prompt_path: MagicMock,
+    mock_prompt_versions: MagicMock,
+    mock_settings: MagicMock,
+    fixture_mirascope_user_settings: MirascopeSettings,
+    fixture_prompt_versions: VersionTextFile,
+    tmp_path: Path,
+):
+    """Tests that `remove` removes a prompt"""
+    version = "0001"
+    version_path = tmp_path / f"{version}_test.py"
+    mock_settings.return_value = fixture_mirascope_user_settings
+    mock_prompt_versions.return_value = fixture_prompt_versions
+    mock_prompt_paths.return_value = None
+    mock_prompt_path.return_value = version_path
+    prompt_file_name = "dan"
 
-#     result = runner.invoke(app, ["remove", prompt_file_name, version])
+    result = runner.invoke(app, ["remove", prompt_file_name, version])
 
-#     mock_remove.assert_called_once_with(mock_prompt_path.return_value)
-#     assert (
-#         result.output.strip() == f"Prompt {prompt_file_name} {version} "
-#         "successfully removed"
-#     )
-#     assert result.exit_code == 0
-
-
-# @patch("mirascope.cli.commands.remove.get_user_mirascope_settings")
-# @patch("mirascope.cli.commands.remove.get_prompt_versions")
-# @patch("os.remove")
-# def test_remove_current_revision_collision(
-#     mock_remove: MagicMock,
-#     mock_prompt_versions: MagicMock,
-#     mock_settings: MagicMock,
-#     fixture_mirascope_user_settings: MirascopeSettings,
-#     fixture_prompt_versions: VersionTextFile,
-# ):
-#     """Tests that `remove` does not remove a prompt if prompt is active."""
-#     mock_settings.return_value = fixture_mirascope_user_settings
-#     mock_prompt_versions.return_value = fixture_prompt_versions
-#     version: str = (
-#         fixture_prompt_versions.current_revision
-#         if fixture_prompt_versions.current_revision
-#         else ""
-#     )
-#     prompt_file_name = "test"
-#     result = runner.invoke(app, ["remove", prompt_file_name, version])
-
-#     assert mock_remove.call_count == 0
-#     assert (
-#         result.output.strip()
-#         == f"Prompt {prompt_file_name} {version} is currently being used. "
-#         "Please switch to another version first."
-#     )
+    mock_remove.assert_called_once_with(mock_prompt_path.return_value)
+    assert (
+        result.output.strip() == f"Prompt {prompt_file_name} {version} "
+        "successfully removed"
+    )
+    assert result.exit_code == 0
 
 
-# @patch("mirascope.cli.commands.remove.get_user_mirascope_settings")
-# @patch("mirascope.cli.commands.remove.get_prompt_versions")
-# @patch("mirascope.cli.commands.remove.find_prompt_path")
-# def test_remove_file_not_found(
-#     mock_prompt_path: MagicMock,
-#     mock_prompt_versions: MagicMock,
-#     mock_settings: MagicMock,
-#     fixture_mirascope_user_settings: MirascopeSettings,
-#     fixture_prompt_versions: VersionTextFile,
-# ):
-#     """Tests that `remove` raises a FileNotFoundError if prompt file is not found."""
-#     mock_prompt_path.return_value = None
-#     mock_settings.return_value = fixture_mirascope_user_settings
-#     mock_prompt_versions.return_value = fixture_prompt_versions
+@patch("mirascope.cli.commands.remove.get_user_mirascope_settings")
+@patch("mirascope.cli.commands.remove.get_prompt_versions")
+@patch("os.remove")
+def test_remove_current_revision_collision(
+    mock_remove: MagicMock,
+    mock_prompt_versions: MagicMock,
+    mock_settings: MagicMock,
+    fixture_mirascope_user_settings: MirascopeSettings,
+    fixture_prompt_versions: VersionTextFile,
+):
+    """Tests that `remove` does not remove a prompt if prompt is active."""
+    mock_settings.return_value = fixture_mirascope_user_settings
+    mock_prompt_versions.return_value = fixture_prompt_versions
+    version: str = (
+        fixture_prompt_versions.current_revision
+        if fixture_prompt_versions.current_revision
+        else ""
+    )
+    prompt_file_name = "test"
+    result = runner.invoke(app, ["remove", prompt_file_name, version])
 
-#     with pytest.raises(FileNotFoundError):
-#         result = runner.invoke(app, ["remove", "test", "0001"], catch_exceptions=False)
-#         assert result.exit_code == 1
+    assert mock_remove.call_count == 0
+    assert (
+        result.output.strip()
+        == f"Prompt {prompt_file_name} {version} is currently being used. "
+        "Please switch to another version first."
+    )
+
+
+@patch("mirascope.cli.commands.remove.get_user_mirascope_settings")
+@patch("mirascope.cli.commands.remove.get_prompt_versions")
+@patch("mirascope.cli.commands.remove.find_prompt_path")
+def test_remove_file_not_found(
+    mock_prompt_path: MagicMock,
+    mock_prompt_versions: MagicMock,
+    mock_settings: MagicMock,
+    fixture_mirascope_user_settings: MirascopeSettings,
+    fixture_prompt_versions: VersionTextFile,
+):
+    """Tests that `remove` raises a FileNotFoundError if prompt file is not found."""
+    mock_prompt_path.return_value = None
+    mock_settings.return_value = fixture_mirascope_user_settings
+    mock_prompt_versions.return_value = fixture_prompt_versions
+
+    with pytest.raises(FileNotFoundError):
+        result = runner.invoke(app, ["remove", "test", "0001"], catch_exceptions=False)
+        assert result.exit_code == 1
