26334419bfeede165be4f13a9731dbecda04a3d9
Brian Park <brian@mirascope.io>
2024-02-23 19:09:14 -0500

    feat: classify example using extract

---
+++ examples/classify.py
@@ -0,0 +1,37 @@
+import enum
+import os
+from typing import List
+
+from pydantic import BaseModel
+
+from mirascope import OpenAIChat
+
+os.environ["OPENAI_API_KEY"] = "YOUR_API_KEY"
+
+
+class Labels(str, enum.Enum):
+    STATEMENT = "statement"
+    QUESTION = "question"
+    AGGRESSIVE = "aggressive"
+    NEUTRAL = "neutral"
+    NICE = "nice"
+
+
+class TextLabels(BaseModel):
+    """A model for labels about some speech."""
+
+    labels: List[Labels]
+
+
+chat = OpenAIChat(model="gpt-3.5-turbo-1106")
+texts = [
+    "Hey buddy, want to grab lunch tomorrow?",
+    "You will pay for what you have done, you pathetic loser.",
+]
+for text in texts:
+    text_info = chat.extract(
+        TextLabels,
+        text,
+        retries=5,
+    )
+    print(text_info)
