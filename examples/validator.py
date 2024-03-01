530ec1e9d085ced091731bbe919d89da55c184cc
Brendan <brendan.kao@sotai.ai>
2024-03-01 10:04:28 -0800

    reverted back to using aftervalidator

--- examples/validator.py
+++ examples/validator.py
@@ -3,15 +3,11 @@
 Note: All validators are defined in the class because updates to the validators will
 affect the output of the model.
 """
-import os
-from typing import Union
 
-from pydantic import (
-    BaseModel,
-    Field,
-    ValidationError,
-    field_validator,
-)
+import os
+from typing import Annotated, Union
+
+from pydantic import AfterValidator, BaseModel, Field, ValidationError
 
 from mirascope import OpenAICallParams, OpenAIChat, Prompt
 
@@ -20,6 +16,28 @@ os.environ["OPENAI_API_KEY"] = "YOUR_API_KEY"
 chat = OpenAIChat()
 
 topic = "baking"
+
+
+def validate_question(question: str) -> str:
+    """Validates the input question."""
+
+    class ValidationInfo(BaseModel):
+        is_question: bool = Field(
+            ..., description="True if the text is a question. False otherwise."
+        )
+        is_experience_relevant: bool = Field(
+            ...,
+            description=f"True if question is related to {topic}. False otherwise.",
+        )
+
+    text = chat.extract(ValidationInfo, question)
+
+    assert text.is_question, f"The text is not a question: {question}"
+    assert (
+        text.is_experience_relevant
+    ), f"The question is not relevant to {topic}: {question}"
+
+    return question
 
 
 class QuestionPrompt(Prompt):
@@ -32,36 +50,16 @@ class QuestionPrompt(Prompt):
     """
 
     topic: str = topic
-    question: str = Field(..., description="The user's question.")
+    question: Annotated[str, AfterValidator(validate_question)] = Field(
+        ..., description="The user's question."
+    )
 
     call_params = OpenAICallParams(model="gpt-3.5-turbo", temperature=0.1)
 
-    @field_validator("question", mode="after")
-    @classmethod
-    def validate_question(cls, question: str) -> str:
-        class Text(BaseModel):
-            is_question: bool = Field(
-                ..., description="True if the text is a question. False otherwise."
-            )
-
-        class Experience(BaseModel):
-            is_experience_relevant: bool = Field(
-                ...,
-                description=f"True if question is related to {topic}. False otherwise.",
-            )
-
-        text = chat.extract(Text, question)
-        experience = chat.extract(Experience, question)
-
-        assert text.is_question, f"The text is not a question: {question}"
-        assert (
-            experience.is_experience_relevant
-        ), f"The question is not relevant to {topic}: {question}"
-
-        return question
-
 
 class HowToGuide(BaseModel):
+    """A how-to guide for the user's question."""
+
     steps: Union[list[str], str] = Field(
         ..., description="The steps to perform the task or answer the question."
     )
@@ -71,5 +69,7 @@ try:
     content = chat.create(QuestionPrompt(question="cake recipe?"))
     guide = chat.extract(HowToGuide, str(content))
     print(guide)
-except ValidationError:
-    print("Either the text was not a question or it is not relevant to the topic.")
+except ValidationError as e:
+    print(e)
+    # Example: The text is not a question: cake recipe
+    # Example: The question is not relevant to baking: how to play guitar
