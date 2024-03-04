785fa133d7f8d24ab53d285919984cc0ffae9802
William Bakst <william@mirascope.io>
2024-03-04 14:03:31 -0800

    fix: enable importing types from mirascope.chat.gemini directly

--- mirascope/chat/gemini/__init__.py
+++ mirascope/chat/gemini/__init__.py
@@ -1,4 +1,4 @@
 """A module for interacting with Google's Gemini models."""
-from . import types
 from .prompt import GeminiPrompt
 from .tools import GeminiTool
+from .types import GeminiCallParams, GeminiCompletion, GeminiCompletionChunk
