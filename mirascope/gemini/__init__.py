511bc4416dc91a8063ad8cfd7c8f77867a5126b1
William Bakst <william@mirascope.io>
2024-03-14 18:43:54 -0700

    fix: add Gemini imports in gemini module base init

--- mirascope/gemini/__init__.py
+++ mirascope/gemini/__init__.py
@@ -1,3 +1,5 @@
 """A module for interacting with Google's Gemini models."""
 from .calls import GeminiCall
 from .extractors import GeminiExtractor
+from .tools import GeminiTool
+from .types import GeminiCallParams, GeminiCallResponse, GeminiCallResponseChunk
