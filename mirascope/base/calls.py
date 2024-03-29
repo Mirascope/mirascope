bbb1be294f10741add6a9cc7bbc2230f5a6bc06f
William Bakst <william@mirascope.io>
2024-03-29 19:51:35 -0400

    fix: yet another attempt

--- mirascope/base/calls.py
+++ mirascope/base/calls.py
@@ -31,9 +31,7 @@ class BaseCall(
 
     api_key: ClassVar[Optional[str]] = None
     base_url: ClassVar[Optional[str]] = None
-    call_params: ClassVar[BaseCallParams] = BaseCallParams[BaseToolT](
-        model="gpt-3.5-turbo-0125"
-    )
+    call_params: ClassVar[BaseCallParams]
 
     @abstractmethod
     def call(self, **kwargs: Any) -> BaseCallResponseT:
