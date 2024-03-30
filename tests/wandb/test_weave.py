3468e1345143a0549cbc01dce39fb7bcbc218bf6
William Bakst <william@mirascope.io>
2024-03-30 07:22:17 -0400

    tests: weave support with extractors

--- tests/wandb/test_weave.py
+++ tests/wandb/test_weave.py
@@ -1,7 +1,9 @@
 """Tests for the Mirascope + Weave integration."""
+from typing import Type
+
 import weave
 
-from mirascope.openai import OpenAICall
+from mirascope.openai import OpenAICall, OpenAIExtractor
 from mirascope.wandb.weave import with_weave
 
 
@@ -11,9 +13,22 @@ class MyCall(OpenAICall):
 
 
 def test_call_with_weave() -> None:
-    my_base_call = MyCall()
-    assert isinstance(my_base_call.call, weave.Op)
-    assert isinstance(my_base_call.call_async, weave.Op)
-    assert isinstance(my_base_call.stream, weave.Op)
-    assert isinstance(my_base_call.stream_async, weave.Op)
-    assert my_base_call.call_params.weave is not None
+    my_call = MyCall()
+    assert isinstance(my_call.call, weave.Op)
+    assert isinstance(my_call.call_async, weave.Op)
+    assert isinstance(my_call.stream, weave.Op)
+    assert isinstance(my_call.stream_async, weave.Op)
+    assert my_call.call_params.weave is not None
+
+
+@with_weave
+class MyExtractor(OpenAIExtractor[int]):
+    extract_schema: Type[int] = int
+    prompt_template = "test"
+
+
+def test_extractor_with_weave() -> None:
+    my_extractor = MyExtractor()
+    assert isinstance(my_extractor.extract, weave.Op)
+    assert isinstance(my_extractor.extract_async, weave.Op)
+    assert my_extractor.call_params.weave is not None
