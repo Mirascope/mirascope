47b49ed1c12badecb925b67e9b6e8e03f1f73ee8
William Bakst <william@mirascope.io>
2024-03-29 22:00:43 -0400

    fix: rename wandb.py to mixins.py in an attempt to fix remote test errors

--- mirascope/wandb/__init__.py
+++ mirascope/wandb/__init__.py
@@ -1,3 +1,3 @@
 """Integrations with Weights & Biases (wandb, weave) tooling."""
-from .wandb import WandbCallMixin, WandbExtractorMixin, trace, trace_error
+from .mixins import WandbCallMixin, WandbExtractorMixin, trace, trace_error
 from .weave import with_weave
