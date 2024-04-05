75864ec0b9e1d5093aba6a00be5d966abd334228
William Bakst <william@mirascope.io>
2024-04-04 22:51:43 -0700

    fix: additional lint issues

--- mirascope/anthropic/tools.py
+++ mirascope/anthropic/tools.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing import Any, Callable, Type, TypeVar
+from typing import Callable, Type, TypeVar
 
 from anthropic.types.beta.tools import ToolParam, ToolUseBlock
 from pydantic import BaseModel
