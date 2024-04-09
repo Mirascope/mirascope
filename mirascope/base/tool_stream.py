04fe810819b56f1bec73e134a725d594f6ea0e72
William Bakst <william@mirascope.io>
2024-04-08 21:32:24 -0700

    feat: stream full tools (not partial yet) using OpenAI (seems to be the only model that supports streaming tools). Still dealing with some mypy issues and need to write tests.

--- mirascope/base/tool_stream.py
+++ mirascope/base/tool_stream.py
@@ -1,27 +1,22 @@
 """A base module for convenience around streaming tools."""
-# from typing import Any, Generator
+from abc import ABC, abstractmethod
+from typing import (
+    AsyncGenerator,
+    Generator,
+    Generic,
+    Literal,
+    Optional,
+    TypeVar,
+    Union,
+    overload,
+)
 
-# from pydantic import BaseModel
+import pydantic
+from pydantic import BaseModel
 
-
-# class BaseToolStream(BaseModel):
-#     """A base class for streaming tools from response chunks."""
-
-#     @classmethod
-#     def from_stream(self, stream: Any) -> Generator[str, None, None]:
-#         for i in range(5):
-#             return str(i)
-
-
-# from abc import ABC, abstractmethod
-# from typing import Any, AsyncGenerator, Generator
-
-# import pydantic
-# from pydantic import BaseModel
-
-# from .tools import BaseTool
-
-# # from .types import BaseCallResponseChunk
+from ..partial import Partial
+from .tools import BaseTool
+from .types import BaseCallResponseChunk
 
 # if int(pydantic.__version__.split(".")[1]) < 7:
 #     raise ImportError(
@@ -30,25 +25,88 @@
 #     )
 
 
-# class BaseToolStream(BaseModel, ABC):
-#     """A base class for streaming tools from response chunks."""
+# BaseToolT = TypeVar("BaseToolT", bound=BaseTool)
+# BaseCallResponseChunkT = TypeVar("BaseCallResponseChunkT", bound=BaseCallResponseChunk)
 
-#     @classmethod
-#     @abstractmethod
-#     def from_stream(
-#         # self, stream: Generator[BaseCallResponseChunkT, None, None]
-#         self,
-#         stream: Any,
-#     ) -> Generator[BaseTool, None, None]:
-#         """Yields partial tools from the given stream of chunks."""
-#         ...  # pragma: no cover
 
-#     @classmethod
-#     @abstractmethod
-#     async def from_async_stream(
-#         # self, async_stream: AsyncGenerator[BaseCallResponseChunkT, None]
-#         self,
-#         async_stream: Any,
-#     ) -> AsyncGenerator[BaseTool, None]:
-#         """Yields partial tools asynchronously from the given async stream of chunks."""
-#         ...  # pragma: no cover
+class BaseToolStream(BaseModel, ABC):
+    """A base class for streaming tools from response chunks."""
+
+    @classmethod
+    @overload
+    def from_stream(
+        cls,
+        stream: Generator[BaseCallResponseChunk, None, None],
+        partial: Literal[True],
+    ) -> Generator[Optional[Partial[BaseTool]], None, None]:
+        ...  # pragma: no cover
+
+    @classmethod
+    @overload
+    def from_stream(
+        cls,
+        stream: Generator[BaseCallResponseChunk, None, None],
+        partial: Literal[False],
+    ) -> Generator[BaseTool, None, None]:
+        ...  # pragma: no cover
+
+    @classmethod
+    @overload
+    def from_stream(
+        cls,
+        stream: Generator[BaseCallResponseChunk, None, None],
+        partial: bool,
+    ) -> Generator[Union[BaseTool, Optional[Partial[BaseTool]]], None, None]:
+        ...  # pragma: no cover
+
+    @classmethod
+    @abstractmethod
+    def from_stream(cls, stream, partial=False):
+        """Yields tools from the given stream of chunks."""
+        ...  # pragma: no cover
+
+    @classmethod
+    @overload
+    async def from_async_stream(
+        cls,
+        async_stream: AsyncGenerator[BaseCallResponseChunk, None],
+        partial: Literal[True],
+    ) -> Generator[Optional[Partial[BaseTool]], None, None]:
+        ...  # pragma: no cover
+
+    @classmethod
+    @overload
+    async def from_async_stream(
+        cls,
+        async_stream: AsyncGenerator[BaseCallResponseChunk, None],
+        partial: Literal[False],
+    ) -> Generator[BaseTool, None, None]:
+        ...  # pragma: no cover
+
+    @classmethod
+    @overload
+    async def from_async_stream(
+        cls,
+        async_stream: AsyncGenerator[BaseCallResponseChunk, None],
+        partial: bool,
+    ) -> Generator[Union[BaseTool, Optional[Partial[BaseTool]]], None, None]:
+        ...  # pragma: no cover
+
+    @classmethod
+    @abstractmethod
+    async def from_async_stream(cls, async_stream, partial=False):
+        """Yields tools asynchronously from the given async stream of chunks."""
+        ...  # pragma: no cover
+
+    ############################## PRIVATE METHODS ###################################
+
+    @classmethod
+    def _check_version_for_partial(cls, partial: bool) -> None:
+        """Checks that the correct version of Pydantic is installed to use partial."""
+        # BUG: https://github.com/pydantic/pydantic/issues/9191
+        # prevents using partial json parsing for now
+        if partial and int(pydantic.__version__.split(".")[1]) < 7:
+            raise ImportError(
+                "You must have `pydantic==^2.7.0` to stream tools. "
+                f"Current version: {pydantic.__version__}"
+            )
--- mirascope/openai/tool_stream.py
+++ mirascope/openai/tool_stream.py
@@ -1,114 +1,168 @@
 """A module for convenience around streaming tools with OpenAI."""
-# from typing import Any, Generator
+from typing import AsyncGenerator, Generator, Literal, Optional, Type, Union, overload
 
-# from ..base.tool_stream import BaseToolStream
+from openai.types.chat.chat_completion_message_tool_call import (
+    ChatCompletionMessageToolCall,
+    Function,
+)
 
-
-# class OpenAIToolStream(BaseToolStream):
-#     """A base class for streaming tools from response chunks."""
-
-#     @classmethod
-#     def stream(self, stream: Any) -> Generator[str, None, None]:
-#         for i in range(5):
-#             yield "OpenAI" + str(i)
-
-
-# from typing import (
-#     Any,
-#     AsyncGenerator,
-#     Generator,
-#     Literal,
-#     Optional,
-#     TypeVar,
-#     Union,
-#     overload,
-# )
-
-# from openai.types.chat.chat_completion_message_tool_call import (
-#     ChatCompletionMessageToolCall,
-#     Function,
-# )
-
-# from ..base.tool_stream import BaseToolStream
-# from ..partial import Partial
-# from .tools import OpenAITool
-# from .types import OpenAICallResponseChunk
-
-# T = TypeVar("T", bound=Union[OpenAITool, Optional[Partial[OpenAITool]]])
+from ..base.tool_stream import BaseToolStream
+from ..partial import Partial
+from .tools import OpenAITool
+from .types import OpenAICallResponseChunk
 
 
-# class OpenAIToolStream(BaseToolStream[OpenAITool]):
-#     """A base class for streaming tools from response chunks."""
+def _handle_chunk(
+    chunk: OpenAICallResponseChunk,
+    current_tool_call: ChatCompletionMessageToolCall,
+    current_tool_type: Optional[Type[OpenAITool]],
+    partial: bool,
+) -> tuple[
+    Optional[OpenAITool], ChatCompletionMessageToolCall, Optional[Type[OpenAITool]]
+]:
+    """Handles a chunk of the stream."""
+    if not chunk.tool_calls or not chunk.tool_types:
+        return None, current_tool_call, current_tool_type
+    tool_call = chunk.tool_calls[0]
+    # Reset on new tool
+    if tool_call.id and tool_call.function is not None:
+        previous_tool_call = current_tool_call.model_copy()
+        previous_tool_type = current_tool_type
+        current_tool_call = ChatCompletionMessageToolCall(
+            id=tool_call.id,
+            function=Function(
+                arguments="",
+                name=tool_call.function.name if tool_call.function.name else "",
+            ),
+            type="function",
+        )
+        current_tool_type = None
+        for tool_type in chunk.tool_types:
+            if tool_type.__name__ == tool_call.function.name:
+                current_tool_type = tool_type
+                break
+        if current_tool_type is None:
+            raise RuntimeError(
+                f"Unknown tool type in stream: {tool_call.function.name}"
+            )
+        if previous_tool_call.id and previous_tool_type is not None:
+            return (
+                previous_tool_type.from_tool_call(previous_tool_call),
+                current_tool_call,
+                current_tool_type,
+            )
 
-#     # @classmethod
-#     # @overload
-#     # def from_stream(
-#     #     self,
-#     #     stream: Generator[OpenAICallResponseChunk, None, None],
-#     #     partial: Literal[True],
-#     # ) -> Generator[Optional[Partial[OpenAITool]], None, None]:
-#     #     ...  # pragma: no cover
+    # Update arguments with each chunk
+    if tool_call.function and tool_call.function.arguments:
+        current_tool_call.function.arguments += tool_call.function.arguments
 
-#     # @classmethod
-#     # @overload
-#     # def from_stream(
-#     #     self,
-#     #     stream: Generator[OpenAICallResponseChunk, None, None],
-#     #     partial: Literal[False],
-#     # ) -> Generator[OpenAITool, None, None]:
-#     #     ...  # pragma: no cover
+    # if partial:
+    #     yield (
+    #         Partial[current_tool_type].from_tool_call(current_tool_call),
+    #         current_tool_call,
+    #         current_tool_type,
+    #     )
 
-#     @classmethod
-#     def from_stream(
-#         self,
-#         stream,
-#         partial: bool = False,
-#     ):
-#         """Yields partial tools from the given stream of chunks.
+    return None, current_tool_call, current_tool_type
 
-#         Raises:
-#             RuntimeError: if a tool in the stream is of an unknown type.
-#         """
-#         current_tool_type = None
-#         current_tool_call = ChatCompletionMessageToolCall(
-#             id="", function=Function(arguments="", name=""), type="function"
-#         )
-#         for chunk in stream:
-#             if not chunk.tool_calls:
-#                 continue
-#             tool_call = chunk.tool_calls[0]
-#             # Reset on new tool
-#             if tool_call.id:
-#                 previous_tool_call = current_tool_call.model_copy()
-#                 current_tool_call = ChatCompletionMessageToolCall(
-#                     id=tool_call.id,
-#                     function=Function(arguments="", name=tool_call.function.name),
-#                     type="function",
-#                 )
-#                 current_tool_type = None
-#                 for tool_type in chunk.tool_types:
-#                     if tool_type.__name__ == tool_call.function.name:
-#                         current_tool_type = tool_type
-#                         break
-#                 if current_tool_type is None:
-#                     raise RuntimeError(
-#                         f"Unknown tool type in stream: {tool_call.function.name}"
-#                     )
-#                 if previous_tool_call.id:
-#                     yield current_tool_type.from_tool_call(previous_tool_call)
 
-#             # Update arguments with each chunk
-#             if tool_call.function:
-#                 current_tool_call.function.arguments += tool_call.function.arguments
+class OpenAIToolStream(BaseToolStream):
+    """A base class for streaming tools from response chunks."""
 
-#             if partial:
-#                 yield Partial[current_tool_type].from_tool_call(current_tool_call)
+    @classmethod
+    @overload
+    def from_stream(
+        cls,
+        stream: Generator[OpenAICallResponseChunk, None, None],
+        partial: Literal[True],
+    ) -> Generator[Optional[Partial[OpenAITool]], None, None]:
+        ...  # pragma: no cover
 
-#     @classmethod
-#     async def from_async_stream(
-#         # self, async_stream: AsyncGenerator[OpenAICallResponseChunk, None]
-#         self,
-#         async_stream: Any,
-#     ) -> AsyncGenerator[Partial[OpenAITool], None]:
-#         """Yields partial tools asynchronously from the given async stream of chunks."""
-#         ...  # pragma: no cover
+    @classmethod
+    @overload
+    def from_stream(
+        cls,
+        stream: Generator[OpenAICallResponseChunk, None, None],
+        partial: Literal[False],
+    ) -> Generator[OpenAITool, None, None]:
+        ...  # pragma: no cover
+
+    @classmethod
+    @overload
+    def from_stream(
+        cls,
+        stream: Generator[OpenAICallResponseChunk, None, None],
+        partial: bool,
+    ) -> Generator[Union[OpenAITool, Optional[Partial[OpenAITool]]], None, None]:
+        ...  # pragma: no cover
+
+    @classmethod
+    def from_stream(
+        cls,
+        stream,
+        partial=False,
+    ):
+        """Yields partial tools from the given stream of chunks.
+
+        Raises:
+            RuntimeError: if a tool in the stream is of an unknown type.
+        """
+        cls._check_version_for_partial(partial)
+        current_tool_call = ChatCompletionMessageToolCall(
+            id="", function=Function(arguments="", name=""), type="function"
+        )
+        current_tool_type = None
+        for chunk in stream:
+            tool, current_tool_call, current_tool_type = _handle_chunk(
+                chunk, current_tool_call, current_tool_type, partial
+            )
+            if tool is not None:
+                yield tool
+        yield current_tool_type.from_tool_call(current_tool_call)
+
+    @classmethod
+    @overload
+    async def from_async_stream(
+        cls,
+        async_stream: AsyncGenerator[OpenAICallResponseChunk, None],
+        partial: Literal[True],
+    ) -> Generator[Optional[Partial[OpenAITool]], None, None]:
+        ...  # pragma: no cover
+
+    @classmethod
+    @overload
+    async def from_async_stream(
+        cls,
+        async_stream: AsyncGenerator[OpenAICallResponseChunk, None],
+        partial: Literal[False],
+    ) -> Generator[OpenAITool, None, None]:
+        ...  # pragma: no cover
+
+    @classmethod
+    @overload
+    async def from_async_stream(
+        cls,
+        async_stream: AsyncGenerator[OpenAICallResponseChunk, None],
+        partial: bool,
+    ) -> Generator[Union[OpenAITool, Optional[Partial[OpenAITool]]], None, None]:
+        ...  # pragma: no cover
+
+    @classmethod
+    async def from_async_stream(cls, async_stream, partial=False):
+        """Yields partial tools from the given stream of chunks asynchronously.
+
+        Raises:
+            RuntimeError: if a tool in the stream is of an unknown type.
+        """
+        cls._check_version_for_partial(partial)
+        current_tool_call = ChatCompletionMessageToolCall(
+            id="", function=Function(arguments="", name=""), type="function"
+        )
+        current_tool_type = None
+        async for chunk in async_stream:
+            tool, current_tool_call, current_tool_type = _handle_chunk(
+                chunk, current_tool_call, current_tool_type, partial
+            )
+            if tool is not None:
+                yield tool
+        yield current_tool_type.from_tool_call(current_tool_call)
