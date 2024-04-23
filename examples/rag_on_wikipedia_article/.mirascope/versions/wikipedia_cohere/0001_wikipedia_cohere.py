8f2804331ea8125e7eb5f3476c8fd4a45f4d8ca3
Brendan <brendan.kao@sotai.ai>
2024-04-23 14:09:21 -0700

    updated prompt template to match mirascope-cli 0.1.1

---
+++ examples/rag_on_wikipedia_article/.mirascope/versions/wikipedia_cohere/0001_wikipedia_cohere.py
@@ -0,0 +1,19 @@
+"""Chroma store for Wikipedia articles."""
+from mirascope.cohere import CohereEmbedder
+from mirascope.pinecone import (
+    PineconeServerlessParams,
+    PineconeSettings,
+    PineconeVectorStore,
+)
+from mirascope.rag import TextChunker
+
+prev_revision_id = None
+revision_id = "0001"
+
+
+class WikipediaStore(PineconeVectorStore):
+    embedder = CohereEmbedder()
+    chunker = TextChunker(chunk_size=1000, chunk_overlap=200)
+    index_name = "wikipedia-cohere-0001"
+    client_settings = PineconeSettings()
+    vectorstore_params = PineconeServerlessParams(cloud="aws", region="us-east-1")
