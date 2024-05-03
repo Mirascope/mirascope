76df37a83aeffead186f6ba64c739121630922a9
Tyler <tylersuard@gmail.com>
2024-05-02 19:26:41 -0700

    add tests

--- mirascope/astra/__init__.py
+++ mirascope/astra/__init__.py
@@ -1,3 +1,10 @@
 """A module for interacting with Astra vectorstores."""
 from .types import AstraParams, AstraQueryResult, AstraSettings
 from .vectorstores import AstraVectorStore
+
+__all__ = [
+    "AstraParams",
+    "AstraQueryResult",
+    "AstraSettings",
+    "AstraVectorStore",
+]
