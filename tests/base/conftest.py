9b0d6fc21ea0be6a63ca94c4c42d5038f0bfcbe7
William Bakst <99370834+willbakst@users.noreply.github.com>
2024-06-02 16:42:43 -0700

    chore: update conftest with_saving decorator for testing updated before/after async handlers

--- tests/base/conftest.py
+++ tests/base/conftest.py
@@ -1,7 +1,7 @@
 """Fixtures for Mirascope's base module tests."""
 
 from typing import Any, Callable
-from unittest.mock import MagicMock
+from unittest.mock import AsyncMock, MagicMock
 
 import pytest
 from pydantic import BaseModel, ConfigDict
@@ -13,6 +13,20 @@ from mirascope.openai.calls import OpenAICall
 def with_saving(cls):
     """Test decorator for saving."""
 
+    def handle_before_call(
+        self: BaseModel,
+        fn: Callable[..., Any],
+        **kwargs: dict[str, Any],
+    ):
+        pass
+
+    async def handle_before_call_async(
+        self: BaseModel,
+        fn: Callable[..., Any],
+        **kwargs: dict[str, Any],
+    ):
+        pass
+
     def handle_after_call(
         self: BaseModel,
         fn: Callable[..., Any],
@@ -22,9 +36,30 @@ def with_saving(cls):
     ):
         pass
 
+    async def handle_after_call_async(
+        self: BaseModel,
+        fn: Callable[..., Any],
+        result: Any,
+        before_result: Any,
+        **kwargs: dict[str, Any],
+    ):
+        pass
+
+    handle_before_call_mock = MagicMock(wraps=handle_before_call)
+    handle_before_call_async_mock = AsyncMock(wraps=handle_before_call_async)
     handle_after_call_mock = MagicMock(wraps=handle_after_call)
-    wrap_mirascope_class_functions(cls, None, handle_after_call_mock)
+    handle_after_call_async_mock = AsyncMock(wraps=handle_after_call_async)
+    wrap_mirascope_class_functions(
+        cls,
+        handle_before_call=handle_before_call_mock,
+        handle_before_call_async=handle_before_call_async_mock,
+        handle_after_call=handle_after_call_mock,
+        handle_after_call_async=handle_after_call_async_mock,
+    )
+    cls.handle_before_call = handle_before_call_mock
+    cls.handle_before_call_async = handle_before_call_async_mock
     cls.handle_after_call = handle_after_call_mock
+    cls.handle_after_call_async = handle_after_call_async_mock
     return cls
 
 
@@ -37,6 +72,9 @@ def fixture_with_saving() -> type[OpenAICall]:
         """
         api_key = "Test"
         model_config = ConfigDict(ignored_types=(MagicMock,))
+        handle_before_call = MagicMock()
+        handle_before_call_async = AsyncMock()
         handle_after_call = MagicMock()
+        handle_after_call_async = AsyncMock()
 
     return TestModel
