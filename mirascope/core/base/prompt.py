24e4a6d00abd8b9b19180f078dabad90246db2bb
William Bakst <william@mirascope.io>
2024-06-21 12:30:59 -0700

    feat: call any decorator from a base prompt instance

--- mirascope/core/base/prompt.py
+++ mirascope/core/base/prompt.py
@@ -1,12 +1,32 @@
 """The `BasePrompt` class for better prompt engineering."""
 
 from textwrap import dedent
-from typing import Any, Callable, ClassVar, TypeVar
+from typing import (
+    Any,
+    AsyncIterable,
+    Awaitable,
+    Callable,
+    ClassVar,
+    Iterable,
+    ParamSpec,
+    TypeVar,
+    overload,
+)
 
 from pydantic import BaseModel
 
-from . import _utils
+from ._utils import BaseType, format_template, parse_prompt_messages
+from .call_response import BaseCallResponse
+from .function_return import BaseFunctionReturn
 from .message_param import BaseMessageParam
+from .stream import BaseStream
+from .stream_async import BaseAsyncStream
+
+_P = ParamSpec("_P")
+_BaseCallResponseT = TypeVar("_BaseCallResponseT", bound=BaseCallResponse)
+_BaseStreamT = TypeVar("_BaseStreamT", bound=BaseStream)
+_BaseAsyncStreamT = TypeVar("_BaseAsyncStreamT", bound=BaseAsyncStream)
+_ResponseModelT = TypeVar("_ResponseModelT", bound=BaseModel | BaseType)
 
 
 class BasePrompt(BaseModel):
@@ -41,11 +61,11 @@ class BasePrompt(BaseModel):
 
     def __str__(self) -> str:
         """Returns the formatted template."""
-        return _utils.format_template(self.prompt_template, self.model_dump())
+        return format_template(self.prompt_template, self.model_dump())
 
     def message_params(self) -> list[BaseMessageParam]:
         """Returns the template as a formatted list of `Message` instances."""
-        return _utils.parse_prompt_messages(
+        return parse_prompt_messages(
             roles=["system", "user", "assistant", "model"],
             template=self.prompt_template,
             attrs=self.model_dump(),
@@ -59,6 +79,144 @@ class BasePrompt(BaseModel):
             "template": dedent(self.prompt_template).strip("\n"),
             "inputs": self.model_dump(),
         }
+
+    @overload
+    def call(
+        self,
+        decorator: Callable[
+            [Callable[..., BaseFunctionReturn]], Callable[..., _BaseCallResponseT]
+        ],
+    ) -> _BaseCallResponseT:
+        ...  # pragma: no cover
+
+    @overload
+    def call(
+        self,
+        decorator: Callable[
+            [Callable[..., BaseFunctionReturn]], Callable[..., _BaseStreamT]
+        ],
+    ) -> _BaseStreamT:
+        ...  # pragma: no cover
+
+    @overload
+    def call(  # type: ignore
+        self,
+        decorator: Callable[
+            [Callable[..., BaseFunctionReturn]], Callable[..., _ResponseModelT]
+        ],
+    ) -> _ResponseModelT:
+        ...  # pragma: no cover
+
+    @overload
+    def call(
+        self,
+        decorator: Callable[
+            [Callable[..., BaseFunctionReturn]],
+            Callable[..., Iterable[_ResponseModelT]],
+        ],
+    ) -> Iterable[_ResponseModelT]:
+        ...  # pragma: no cover
+
+    def call(
+        self,
+        decorator: Callable[
+            [Callable[..., BaseFunctionReturn]],
+            Callable[..., _BaseCallResponseT],
+        ]
+        | Callable[
+            [Callable[..., BaseFunctionReturn]],
+            Callable[..., _BaseStreamT],
+        ]
+        | Callable[
+            [Callable[..., BaseFunctionReturn]],
+            Callable[..., _ResponseModelT],
+        ]
+        | Callable[
+            [Callable[..., BaseFunctionReturn]],
+            Callable[..., Iterable[_ResponseModelT]],
+        ],
+    ) -> (
+        _BaseCallResponseT | _BaseStreamT | _ResponseModelT | Iterable[_ResponseModelT]
+    ):
+        """Returns the response of calling the API of the provided decorator."""
+
+        @decorator
+        def _call() -> BaseFunctionReturn:
+            return {"messages": self.message_params()}
+
+        return _call()
+
+    @overload
+    async def call_async(
+        self,
+        decorator: Callable[
+            [Callable[..., Awaitable[BaseFunctionReturn]]],
+            Callable[..., Awaitable[_BaseCallResponseT]],
+        ],
+    ) -> _BaseCallResponseT:
+        ...  # pragma: no cover
+
+    @overload
+    async def call_async(
+        self,
+        decorator: Callable[
+            [Callable[..., Awaitable[BaseFunctionReturn]]],
+            Callable[..., Awaitable[_BaseAsyncStreamT]],
+        ],
+    ) -> _BaseAsyncStreamT:
+        ...  # pragma: no cover
+
+    @overload
+    async def call_async(
+        self,
+        decorator: Callable[
+            [Callable[..., Awaitable[BaseFunctionReturn]]],
+            Callable[..., Awaitable[_ResponseModelT]],
+        ],
+    ) -> _ResponseModelT:
+        ...  # pragma: no cover
+
+    @overload
+    async def call_async(
+        self,
+        decorator: Callable[
+            [Callable[..., Awaitable[BaseFunctionReturn]]],
+            Callable[..., Awaitable[AsyncIterable[_ResponseModelT]]],
+        ],
+    ) -> AsyncIterable[_ResponseModelT]:
+        ...  # pragma: no cover
+
+    async def call_async(
+        self,
+        decorator: Callable[
+            [Callable[..., Awaitable[BaseFunctionReturn]]],
+            Callable[..., Awaitable[_BaseCallResponseT]],
+        ]
+        | Callable[
+            [Callable[..., Awaitable[BaseFunctionReturn]]],
+            Callable[..., Awaitable[_BaseAsyncStreamT]],
+        ]
+        | Callable[
+            [Callable[..., Awaitable[BaseFunctionReturn]]],
+            Callable[..., Awaitable[_ResponseModelT]],
+        ]
+        | Callable[
+            [Callable[..., Awaitable[BaseFunctionReturn]]],
+            Callable[..., Awaitable[AsyncIterable[_ResponseModelT]]],
+        ],
+    ) -> (
+        _BaseCallResponseT
+        | _BaseAsyncStreamT
+        | _ResponseModelT
+        | AsyncIterable[_ResponseModelT]
+    ):
+        """Returns the response of async calling the API of the provided decorator."""
+
+        @decorator
+        async def _call_async() -> BaseFunctionReturn:
+            return {"messages": self.message_params()}
+
+        return await _call_async()
 
 
 BasePromptT = TypeVar("BasePromptT", bound=BasePrompt)
