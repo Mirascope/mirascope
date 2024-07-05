639ee484f58e4d5b80bd7532ea1236e5f12c54fc
William Bakst <william@mirascope.io>
2024-07-05 13:28:36 -0400

    fix: some import issues and tags needing to be a set not a dict

--- mirascope/core/base/_utils/get_tags.py
+++ mirascope/core/base/_utils/get_tags.py
@@ -5,9 +5,9 @@ from typing import Callable
 from ..dynamic_config import BaseDynamicConfig
 
 
-def get_tags(fn: Callable, dynamic_config: BaseDynamicConfig):
+def get_tags(fn: Callable, dynamic_config: BaseDynamicConfig) -> set[str]:
     """Get the tags from the function and merge with any dynamic tags."""
     tags = fn.__annotations__.get("tags", {})
     if dynamic_tags := getattr(dynamic_config, "tags", {}):
         tags = tags | dynamic_tags
-    return tags
+    return set(tags)
