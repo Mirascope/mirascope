e8a7e6ca112a1e90f6af202dc4c700c21b1a3b5f
William Bakst <william@mirascope.io>
2024-07-05 13:31:22 -0400

    chore: update get_tags to just return set(tags)

--- mirascope/core/base/_utils/get_tags.py
+++ mirascope/core/base/_utils/get_tags.py
@@ -7,7 +7,7 @@ from ..dynamic_config import BaseDynamicConfig
 
 def get_tags(fn: Callable, dynamic_config: BaseDynamicConfig) -> set[str]:
     """Get the tags from the function and merge with any dynamic tags."""
-    tags: set[str] = fn.__annotations__.get("tags", set())
-    if dynamic_tags := getattr(dynamic_config, "tags", set()):
+    tags = fn.__annotations__.get("tags", {})
+    if dynamic_tags := getattr(dynamic_config, "tags", {}):
         tags = tags | dynamic_tags
     return set(tags)
