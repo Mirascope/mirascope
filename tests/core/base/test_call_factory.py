6d99c419c05d2780c5e824c7ab81bf8d325ebed0
William Bakst <william@mirascope.io>
2024-07-18 16:19:22 -0700

    tests: base/core/call_factory.py

---
+++ tests/core/base/test_call_factory.py
@@ -0,0 +1,163 @@
+"""Tests the `call_factory` module."""
+
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+from mirascope.core.base.call_factory import call_factory
+
+
+@pytest.fixture()
+def mock_call_factory_kwargs() -> dict:
+    """Returns the mock kwargs for the `call_factory` function."""
+    return {
+        "TCallResponse": MagicMock,
+        "TCallResponseChunk": MagicMock,
+        "TDynamicConfig": MagicMock,
+        "TToolType": MagicMock,
+        "TStream": MagicMock,
+        "TCallParams": MagicMock,
+        "default_call_params": MagicMock(),
+        "setup_call": MagicMock(),
+        "get_json_output": MagicMock(),
+        "handle_stream": MagicMock(),
+        "handle_stream_async": MagicMock(),
+    }
+
+
+@patch("mirascope.core.base.call_factory.create_factory", new_callable=MagicMock)
+@patch("mirascope.core.base.call_factory.partial", new_callable=MagicMock)
+def test_call_factory_create(
+    mock_partial: MagicMock,
+    mock_create_factory: MagicMock,
+    mock_call_factory_kwargs: dict,
+) -> None:
+    """Tests the `create_factory` route for the `call_factory` method."""
+    call = call_factory(**mock_call_factory_kwargs)
+    create_kwargs = {
+        "model": "model",
+        "tools": [],
+        "output_parser": MagicMock(),
+        "json_mode": True,
+        "client": MagicMock(),
+        "call_params": MagicMock(),
+    }
+    _ = call(**create_kwargs)
+    mock_create_factory.assert_called_once_with(
+        TCallResponse=mock_call_factory_kwargs["TCallResponse"],
+        setup_call=mock_call_factory_kwargs["setup_call"],
+    )
+    mock_partial.assert_called_once_with(
+        mock_create_factory.return_value, **create_kwargs
+    )
+
+
+@patch("mirascope.core.base.call_factory.stream_factory", new_callable=MagicMock)
+@patch("mirascope.core.base.call_factory.partial", new_callable=MagicMock)
+def test_call_factory_stream(
+    mock_partial: MagicMock,
+    mock_stream_factory: MagicMock,
+    mock_call_factory_kwargs: dict,
+) -> None:
+    """Tests the `stream_factory` route for the `call_factory` method."""
+    call = call_factory(**mock_call_factory_kwargs)
+    stream_kwargs = {
+        "model": "model",
+        "tools": [],
+        "json_mode": False,
+        "client": MagicMock(),
+        "call_params": MagicMock(),
+    }
+    _ = call(stream=True, **stream_kwargs)
+    mock_stream_factory.assert_called_once_with(
+        TCallResponse=mock_call_factory_kwargs["TCallResponse"],
+        TStream=mock_call_factory_kwargs["TStream"],
+        setup_call=mock_call_factory_kwargs["setup_call"],
+        handle_stream=mock_call_factory_kwargs["handle_stream"],
+        handle_stream_async=mock_call_factory_kwargs["handle_stream_async"],
+    )
+    mock_partial.assert_called_once_with(
+        mock_stream_factory.return_value, **stream_kwargs
+    )
+
+
+@patch("mirascope.core.base.call_factory.extract_factory", new_callable=MagicMock)
+@patch("mirascope.core.base.call_factory.partial", new_callable=MagicMock)
+def test_call_factory_extract(
+    mock_partial: MagicMock,
+    mock_extract_factory: MagicMock,
+    mock_call_factory_kwargs: dict,
+) -> None:
+    """Tests the `extract_factory` route for the `call_factory` method."""
+    call = call_factory(**mock_call_factory_kwargs)
+    extract_kwargs = {
+        "model": "model",
+        "response_model": MagicMock,
+        "json_mode": False,
+        "client": MagicMock(),
+        "call_params": MagicMock(),
+    }
+    _ = call(**extract_kwargs)
+    mock_extract_factory.assert_called_once_with(
+        TCallResponse=mock_call_factory_kwargs["TCallResponse"],
+        TToolType=mock_call_factory_kwargs["TToolType"],
+        setup_call=mock_call_factory_kwargs["setup_call"],
+        get_json_output=mock_call_factory_kwargs["get_json_output"],
+    )
+    mock_partial.assert_called_once_with(
+        mock_extract_factory.return_value, **extract_kwargs
+    )
+
+
+@patch(
+    "mirascope.core.base.call_factory.structured_stream_factory", new_callable=MagicMock
+)
+@patch("mirascope.core.base.call_factory.partial", new_callable=MagicMock)
+def test_call_factory_structured_stream(
+    mock_partial: MagicMock,
+    mock_structured_stream_factory: MagicMock,
+    mock_call_factory_kwargs: dict,
+) -> None:
+    """Tests the `structured_stream_factory` route for the `call_factory` method."""
+    call = call_factory(**mock_call_factory_kwargs)
+    structured_stream_kwargs = {
+        "model": "model",
+        "response_model": MagicMock,
+        "json_mode": False,
+        "client": MagicMock(),
+        "call_params": MagicMock(),
+    }
+    _ = call(stream=True, **structured_stream_kwargs)
+    mock_structured_stream_factory.assert_called_once_with(
+        TCallResponse=mock_call_factory_kwargs["TCallResponse"],
+        TCallResponseChunk=mock_call_factory_kwargs["TCallResponseChunk"],
+        TStream=mock_call_factory_kwargs["TStream"],
+        TToolType=mock_call_factory_kwargs["TToolType"],
+        setup_call=mock_call_factory_kwargs["setup_call"],
+        get_json_output=mock_call_factory_kwargs["get_json_output"],
+    )
+    mock_partial.assert_called_once_with(
+        mock_structured_stream_factory.return_value, **structured_stream_kwargs
+    )
+
+
+def test_call_decorator_invalid_output_parser_with_stream(
+    mock_call_factory_kwargs: dict,
+) -> None:
+    """Tests a ValueError is raised if `output_parser` is provided and `stream=True`."""
+    call = call_factory(**mock_call_factory_kwargs)
+    with pytest.raises(
+        ValueError, match="Cannot use `output_parser` with `stream=True`"
+    ):
+        call("model", stream=True, output_parser=MagicMock())
+
+
+def test_call_decorator_invalid_output_parser_with_response_model(
+    mock_call_factory_kwargs: dict,
+) -> None:
+    """Tests a ValueError is raised if `output_parser` and `response_model` are set."""
+    call = call_factory(**mock_call_factory_kwargs)
+    with pytest.raises(
+        ValueError, match="Cannot use both `response_model` and `output_parser`"
+    ):
+        call("model", response_model=MagicMock, output_parser=MagicMock())
