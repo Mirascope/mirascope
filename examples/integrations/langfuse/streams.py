c49e04ba397aeb795488832bcf7907d4443f5285
Brendan <brendan.kao@sotai.ai>
2024-08-15 17:50:47 -0700

    forgot 1

--- examples/integrations/langfuse/streams.py
+++ examples/integrations/langfuse/streams.py
@@ -2,7 +2,7 @@ from mirascope.core import openai, prompt_template
 from mirascope.integrations.langfuse import with_langfuse
 
 
-@with_langfuse
+@with_langfuse()
 @openai.call(
     model="gpt-4o-mini",
     stream=True,
