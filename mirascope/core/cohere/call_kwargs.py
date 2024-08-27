2cb4b38ee2925211ca99966198e50964818bf3db
Koudai Aono <koudai@mirascope.io>
2024-08-27 13:58:55 +0900

    fix: Fix message type

--- mirascope/core/cohere/call_kwargs.py
+++ mirascope/core/cohere/call_kwargs.py
@@ -1,7 +1,5 @@
 """This module contains the type definition for the Cohere call keyword arguments."""
 
-from cohere.types import ChatMessage
-
 from ..base import BaseCallKwargs
 from .call_params import CohereCallParams
 from .tool import CohereTool
@@ -9,4 +7,4 @@ from .tool import CohereTool
 
 class CohereCallKwargs(CohereCallParams, BaseCallKwargs[CohereTool]):
     model: str
-    message: ChatMessage
+    message: str
