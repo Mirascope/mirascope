406386763dcb8c45f4d4efbe18dd42185c2b9f7b
Koudai Aono <koudai@mirascope.io>
2024-09-17 18:03:27 +0900

    Improve Messages

---
+++ mirascope/core/base/messages.py
@@ -0,0 +1,130 @@
+"""This module contains the base class for message parameters."""
+
+from __future__ import annotations
+
+from collections.abc import Sequence
+from typing import TYPE_CHECKING, Literal
+
+from pydantic import BaseModel
+
+from ._utils._get_message_params import (
+    get_content_from_message_sequence,
+)
+
+if TYPE_CHECKING:
+    try:
+        from PIL import Image
+    except ImportError:
+        Image = object
+
+
+class TextPart(BaseModel):
+    """A content part for text.
+
+    Attributes:
+        type: Always "text"
+        text: The text content
+    """
+
+    type: Literal["text"]
+    text: str
+
+
+class CacheControlPart(BaseModel):
+    """A `TextPart` with cache control.
+
+    This part is currently only available with Anthropic. For more details, see:
+    https://docs.anthropic.com/en/docs/build-with-claude/prompt-caching
+
+    Attributes:
+        type: Always "cache_control"
+        cache_type: Currently only "ephemeral" is available.
+    """
+
+    type: Literal["cache_control"]
+    cache_type: str
+
+
+class ImagePart(BaseModel):
+    """A content part for images.
+
+    Attributes:
+        type: Always "image"
+        media_type: The media type (e.g. image/jpeg)
+        image: The raw image bytes
+        detail: (Optional) The detail to use for the image (supported by OpenAI)
+    """
+
+    type: Literal["image"]
+    media_type: str
+    image: bytes
+    detail: str | None
+
+
+class AudioPart(BaseModel):
+    """A content part for audio.
+
+    Attributes:
+        type: Always "audio"
+        media_type: The media type (e.g. audio/wav)
+        audio: The raw audio bytes
+    """
+
+    type: Literal["audio"]
+    media_type: str
+    audio: bytes
+
+
+class BaseMessageParam(BaseModel):
+    """A base class for message parameters.
+
+    usage docs: learn/prompts.md#message-roles
+
+    Attributes:
+        role: The role of the message (e.g. "system", "user", "assistant")
+        content: The content of the message
+    """
+
+    role: str
+    content: str | list[TextPart | ImagePart | AudioPart | CacheControlPart]
+
+
+def _get_content_from_message(
+    content: str | Sequence[str | Image.Image | TextPart | ImagePart | AudioPart],
+) -> str | list[TextPart | ImagePart | AudioPart | CacheControlPart]:
+    if isinstance(content, str):
+        return content
+    return get_content_from_message_sequence(content)
+
+
+class Messages:
+    Type = (
+        str
+        | Sequence[str | Image.Image | TextPart | ImagePart | AudioPart]
+        | list[BaseMessageParam]
+        | BaseMessageParam
+    )
+
+    class System(BaseMessageParam):
+        def __init__(
+            self,
+            content: str | Sequence[str | Image.Image | TextPart | ImagePart | AudioPart],
+        ) -> None:
+            super().__init__(content=_get_content_from_message(content), role="system")
+
+    class User(BaseMessageParam):
+        def __init__(
+            self,
+            content: str | Sequence[str | Image.Image | TextPart | ImagePart | AudioPart]
+        ) -> None:
+            super().__init__(content=_get_content_from_message(content), role="user")
+
+    class Assistant(BaseMessageParam):
+        def __init__(
+            self,
+            content: str | Sequence[str | Image.Image | TextPart | ImagePart | AudioPart]
+        ) -> None:
+            super().__init__(
+                content=_get_content_from_message(content),
+                role="assistant",
+            )
