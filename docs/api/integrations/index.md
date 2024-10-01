3c32c4a0b39a1bec8e2c7e26d0d48fed899542d4
William Bakst <william@mirascope.io>
2024-09-30 18:25:05 -0700

    docs: update json mode section

--- docs/api/integrations/index.md
+++
@@ -1,3 +0,0 @@
-# Mirascope Integrations
-
-This section provides a reference for the integrations that Mirascope currently supports, which can further help make your systems more robust.
---
+++ examples/learn/json_mode/basic_usage/vertex/messages.py
@@ -0,0 +1,13 @@
+import json
+
+from mirascope.core import Messages, vertex
+
+
+@vertex.call("gemini-1.5-flash", json_mode=True)
+def get_book_info(book_title: str) -> Messages.Type:
+    return Messages.User(f"Provide the author and genre of {book_title}")
+
+
+response = get_book_info("The Name of the Wind")
+print(json.loads(response.content))
+# Output: {'author': 'Patrick Rothfuss', 'genre': 'Fantasy'}
