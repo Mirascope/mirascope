11232ebbf990523feb94edb695dbaff9a38c2362
Koudai Aono <koudai@mirascope.io>
2024-10-09 15:14:48 +0900

    fix: Fix broken protocol types

--- mirascope/core/base/structured_stream.py
+++ mirascope/core/base/structured_stream.py
@@ -171,7 +171,7 @@ def structured_stream_factory(  # noqa: ANN201
         model: str,
         response_model: type[_ResponseModelT],
         json_mode: bool,
-        client: _SameSyncAndAsyncClientT | _SyncBaseClientT | None,
+        client: _SameSyncAndAsyncClientT | _AsyncBaseClientT | None,
         call_params: _BaseCallParamsT,
     ) -> Callable[
         _P,
@@ -184,7 +184,7 @@ def structured_stream_factory(  # noqa: ANN201
         model: str,
         response_model: type[_ResponseModelT],
         json_mode: bool,
-        client: _SameSyncAndAsyncClientT | _SyncBaseClientT | None,
+        client: _SameSyncAndAsyncClientT | _SyncBaseClientT | _AsyncBaseClientT | None,
         call_params: _BaseCallParamsT,
     ) -> Callable[
         _P,
