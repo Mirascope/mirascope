bdd4e82f22506ec3044517a5c25849f2c858ba04
William Bakst <99370834+willbakst@users.noreply.github.com>
2024-11-01 10:43:00 -0700

    Revert "Revert "feat: Add Mirascope tools""

---
+++ mirascope/tools/__init__.py
@@ -0,0 +1,12 @@
+from .web._duckduckgo import DuckDuckGoSearch, DuckDuckGoSearchConfig
+from .web._parse_url_content import ParseURLConfig, ParseURLContent
+from .web._requests import Requests, RequestsConfig
+
+__all__ = [
+    "DuckDuckGoSearch",
+    "DuckDuckGoSearchConfig",
+    "ParseURLContent",
+    "ParseURLConfig",
+    "Requests",
+    "RequestsConfig",
+]
