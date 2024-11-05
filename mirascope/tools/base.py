8711f53c1afc1f2b366ca6008a88cf8141fbf440
Koudai Aono <koudai@mirascope.io>
2024-11-05 11:45:30 +0900

    feat: update create_tools

--- mirascope/tools/base.py
+++ mirascope/tools/base.py
@@ -1,14 +1,13 @@
 from __future__ import annotations
 
 import inspect
-import uuid
 from abc import ABC
 from typing import ClassVar, Generic, TypeVar, cast
 
 from pydantic import BaseModel, ConfigDict, create_model
 
 from mirascope.core import BaseTool, BaseToolKit
-from mirascope.core.base.toolkit import is_toolkit_tool
+from mirascope.core.base._utils import DEFAULT_TOOL_DOCSTRING
 
 
 class _ToolConfig(BaseModel, ABC):
@@ -55,6 +54,7 @@ class ConfigurableTool(
             cls.__name__,
             __base__=cls,
             __module__=cls.__module__,
+            __doc__=cls.__doc__ if cls.__doc__ else DEFAULT_TOOL_DOCSTRING,
         )
         new_model.__prompt_usage_description__ = cls.__prompt_usage_description__
         new_model.__config__ = config
@@ -80,29 +80,14 @@ class ConfigurableToolKit(BaseToolKit, Generic[_ToolConfigT], ABC):
     __prompt_usage_description__: ClassVar[str]
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
-    @classmethod
-    def from_config(
-        cls: type[_ConfigurableToolKitT], config: _ToolConfigT
-    ) -> type[_ConfigurableToolKitT]:
-        """Create tool class with custom configuration"""
-
-        # ClassVar cannot get TypeVar Type. So, we need the comment to ignore the error.
-        config = cls.__config__.model_validate(config)  # pyright: ignore [reportAssignmentType]
-        new_model = create_model(
-            cls.__name__,
-            __base__=cls,
-            __module__=cls.__module__,
-            __namespace__=f"{cls.__namespace__}_{uuid.uuid4()}",
-        )
-        new_model.__prompt_usage_description__ = cls.__prompt_usage_description__
-        new_model.__config__ = config
-
-        for key in dir(new_model):
-            attr = getattr(new_model, key)
-            if not is_toolkit_tool(attr):
-                continue
-            attr.__config__ = config
-        return new_model
+    def create_tools(self) -> list[type[ConfigurableTool[_ToolConfigT, _ToolSchemaT]]]:
+        """The method to create the tools."""
+        return [
+            tool.from_config(self.__config__)
+            if issubclass(tool, ConfigurableTool)
+            else tool
+            for tool in super().create_tools()
+        ]
 
     @classmethod
     def usage_description(cls) -> str:
