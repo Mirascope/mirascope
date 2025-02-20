b78e40cb8fa58b5733dc5f23e521d3ee64873376
Dandelion Mané <dandelion@dandelion.art>
2025-02-20 12:55:14 -0800

    feat: convert agents.md to provider agnostic calls

---
+++ examples/learn/agents/human_in_the_loop/string_template.py
@@ -0,0 +1,58 @@
+from mirascope import (
+    BaseDynamicConfig,
+    Messages,
+    llm,
+    BaseMessageParam,
+    prompt_template,
+)
+from pydantic import BaseModel
+
+
+class Librarian(BaseModel):
+    history: list[BaseMessageParam] = []
+
+    def _ask_for_help(self, question: str) -> str:
+        """Asks for help from an expert."""
+        print("[Assistant Needs Help]")
+        print(f"[QUESTION]: {question}")
+        answer = input("[ANSWER]: ")
+        print("[End Help]")
+        return answer
+
+    @llm.call(provider="openai", model="gpt-4o-mini")
+    @prompt_template(
+        """
+        SYSTEM: You are a librarian
+        MESSAGES: {self.history}
+        USER: {query}
+        """
+    )
+    def _call(self, query: str) -> BaseDynamicConfig:
+        return {"tools": [self._ask_for_help]}
+
+    def _step(self, query: str) -> str:
+        if query:
+            self.history.append(Messages.User(query))
+        response = self._call(query)
+        self.history.append(response.message_param)
+        tools_and_outputs = []
+        if tools := response.tools:
+            for tool in tools:
+                print(f"[Calling Tool '{tool._name()}' with args {tool.args}]")
+                tools_and_outputs.append((tool, tool.call()))
+            self.history += response.tool_message_params(tools_and_outputs)
+            return self._step("")
+        else:
+            return response.content
+
+    def run(self) -> None:
+        while True:
+            query = input("(User): ")
+            if query in ["exit", "quit"]:
+                break
+            print("(Assistant): ", end="", flush=True)
+            step_output = self._step(query)
+            print(step_output)
+
+
+Librarian().run()
---
+++ examples/learn/agents/tools/string_template.py
@@ -0,0 +1,65 @@
+import json
+
+from mirascope import (
+    BaseDynamicConfig,
+    Messages,
+    llm,
+    BaseMessageParam,
+    prompt_template,
+)
+from pydantic import BaseModel
+
+
+class Book(BaseModel):
+    title: str
+    author: str
+
+
+class Librarian(BaseModel):
+    history: list[BaseMessageParam] = []
+    library: list[Book] = [
+        Book(title="The Name of the Wind", author="Patrick Rothfuss"),
+        Book(title="Mistborn: The Final Empire", author="Brandon Sanderson"),
+    ]
+
+    def _available_books(self) -> str:
+        """Returns the list of books available in the library."""
+        return json.dumps([book.model_dump() for book in self.library])
+
+    @llm.call(provider="openai", model="gpt-4o-mini")
+    @prompt_template(
+        """
+        SYSTEM: You are a librarian
+        MESSAGES: {self.history}
+        USER: {query}
+        """
+    )
+    def _call(self, query: str) -> BaseDynamicConfig:
+        return {"tools": [self._available_books]}
+
+    def _step(self, query: str) -> str:
+        if query:
+            self.history.append(Messages.User(query))
+        response = self._call(query)
+        self.history.append(response.message_param)
+        tools_and_outputs = []
+        if tools := response.tools:
+            for tool in tools:
+                print(f"[Calling Tool '{tool._name()}' with args {tool.args}]")
+                tools_and_outputs.append((tool, tool.call()))
+            self.history += response.tool_message_params(tools_and_outputs)
+            return self._step("")
+        else:
+            return response.content
+
+    def run(self) -> None:
+        while True:
+            query = input("(User): ")
+            if query in ["exit", "quit"]:
+                break
+            print("(Assistant): ", end="", flush=True)
+            step_output = self._step(query)
+            print(step_output)
+
+
+Librarian().run()
