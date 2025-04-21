8d149d9ac42e1c3bdfe325f0cd535547d0b75c40
William Bakst <william@mirascope.io>
2025-04-21 16:57:54 -0700

    tests: update missing snippets to fail since xai is missing

--- tests/docs/test_missing_snippets.py
+++ tests/docs/test_missing_snippets.py
@@ -2,16 +2,30 @@ from pathlib import Path
 
 from docs.find_missing_snippets import all_missing_snippets
 
+PROVIDERS = [
+    "anthropic",
+    "azure",
+    "bedrock",
+    "cohere",
+    "gemini",
+    "google",
+    "groq",
+    "litellm",
+    "mistral",
+    "openai",
+    "vertex",
+    "xai",
+]
+
 
 def test_no_missing_snippets():
-    missing_snippets = []
-    xai_missing_snippets = 0
+    missing_snippets = {provider: [] for provider in PROVIDERS}
     for snippet in all_missing_snippets(Path(".")):
         str_path = str(snippet.path)
-        if "xai" in str_path:
-            # TODO: Remove this once the xai snippets are fixed (after finishing #811)
-            xai_missing_snippets += 1
-            continue
-        missing_snippets.append(snippet)  # pragma: no cover
-    assert xai_missing_snippets == 33
-    assert len(missing_snippets) == 0
+        for provider in missing_snippets:
+            if provider in str_path:
+                missing_snippets[provider].append(snippet)
+                break
+    assert all(
+        len(missing_snippets) == 0 for _, missing_snippets in missing_snippets.items()
+    )
