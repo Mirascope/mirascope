d0d522c90ff99d47bc72067c50abba76145276b7
William Bakst <william@mirascope.io>
2025-04-23 09:47:34 -0700

    feat(2): add stub interface for FromCallArgs

--- mirascope/experimental/v2/llm/response_formatting.py
+++ mirascope/experimental/v2/llm/response_formatting.py
@@ -70,6 +70,17 @@ class TextParserFn(Protocol[CovariantT]):
         ...
 
 
+class FromCallArgs:
+    """A marker class for indicating that a field is a call argument.
+
+    This ensures that the LLM call does not attempt to generate this field. Instead, it
+    will populate this field with the call argument with a matching name.
+
+    This is useful for colocating e.g. validation of a generated output against and
+    input argument (such as the length of an output given a number input).
+    """
+
+
 @dataclass
 class ResponseFormat(Generic[T]):
     """Class representing a structured output format for LLM responses.
