e19a05035b9424fec2cf0e6dc40da85705cc3be9
Dandelion Mané <dandelion@dandelion.art>
2025-06-25 00:56:34 -0700

    chore: another pass on prompts and examples

--- examples/prompts/intro.py
+++ examples/prompts/intro.py
@@ -1,46 +1,23 @@
-import inspect
-
 from mirascope import llm
 
-hardcoded_prompt = [
-    llm.messages.system(
-        inspect.cleandoc(
-            """
-            You are a book recommendation agent. Recommend a book to the user based on
-            their request; however ensure that the book is age-appropriate for a 13
-            year old reader.
-            """
-        )
-    ),
-    llm.messages.user("I want to read a fantasy book with music, magic, and dragons!"),
-]
+hardcoded_prompt = [llm.messages.user("Please recommend a book")]
+
+fantasy_prompt = [llm.messages.user("Please recommend a fantasy book")]
+
+scifi_prompt = [llm.messages.user("Please recommend a science fiction book")]
 
 
-def prompt(age: int, genre: str, interests: str) -> list[llm.Message]:
-    return [
-        llm.messages.system(
-            inspect.cleandoc(
-                f"""
-                You are a book recommendation agent. Recommend a book to the user based on
-                their request; however ensure that the book is age-appropriate for a {age}
-                year old reader.
-                """
-            )
-        ),
-        llm.messages.user(f"I want to read a {genre} book with {interests}!"),
-    ]
+def genre_prompt(genre: str) -> list[llm.Message]:
+    return [llm.messages.user(f"Please recommend a {genre} book")]
 
 
-@llm.prompt(
-    """
-    [SYSTEM] 
-    You are a book recommendation agent. Recommend a book to the user based on
-    their request; however ensure that the book is age-appropriate for a {{ age }}
-    year old reader.
-
-    [USER]
-    I want to read a {{ genre }} book with {{ interests }}!
-    """
-)
-def prompt_from_template(age: int, genre: str, interests: str):
+# Template Style
+@llm.prompt("Please recommend a {{ genre }} book")
+def genre_prompt_from_template(genre: str):
     pass
+
+
+# Functional Style
+@llm.prompt()
+def genre_prompt_functional(genre: str) -> str:
+    return f"Please recommend a {genre} book"
