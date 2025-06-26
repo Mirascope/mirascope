3fbf7ba4badab16ece5254f701cf45ea0bc470e3
Dandelion Mané <dandelion@dandelion.art>
2025-06-26 15:51:35 -0700

    fix: don't emit example js files

--- typescript/examples/messages/shorthand.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mirascope_1 = require("mirascope");
-const systemMessage = mirascope_1.llm.messages.system('You are a helpful librarian who recommends books.');
-const userMessage = mirascope_1.llm.messages.user('Can you recommend a good fantasy book?');
-const assistantMessage = mirascope_1.llm.messages.assistant('I recommend "The Name of the Wind" by Patrick Rothfuss.');
