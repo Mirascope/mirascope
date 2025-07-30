cc7fe5c12a4e6b942aa77c4dbf32e0ba7f7b1b1c
Dandelion Mané <dandelion@dandelion.art>
2025-07-29 23:43:40 -0700

    chore: nuke all non-sazed examples

--- python/examples/calls/system_injection.py
+++
@@ -1,9 +0,0 @@
-from mirascope import llm
-
-
-# Unsafe - Do not do this!
-def unsafe_recommendation(genre: str):
-    return [
-        llm.messages.system(f"You are a librarian who recommends books in {genre}."),
-        llm.messages.user("Please recommend a book!"),
-    ]
