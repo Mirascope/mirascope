b5e1c6815f11ea6744b7d97b014dc1a49efe6e45
Dandelion Mané <dandelion@dandelion.art>
2025-07-29 23:43:40 -0700

    chore: nuke all non-sazed examples

--- python/examples/calls/custom_params.py
+++
@@ -1,8 +0,0 @@
-from mirascope import llm
-
-custom_params = llm.clients.OpenAIParams()  # [!code highlight]
-
-
-@llm.call("openai:gpt-4o-mini", **custom_params)  # [!code highlight]
-def recommend_book(genre: str):
-    return f"Recommend a {genre} book."
