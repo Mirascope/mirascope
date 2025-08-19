91d2eaedbe9e1812e8fc08992574d7f581f803f7
Dandelion Mané <dandelion@dandelion.art>
2025-08-18 20:08:17 -0700

    feat: implement llm.formatting.ensure_formattable

--- python/mirascope/llm/formatting/__init__.py
+++ python/mirascope/llm/formatting/__init__.py
@@ -5,9 +5,8 @@ The `@format` decorator can be applied to classes to specify how LLM
 outputs should be structured and parsed.
 """
 
-from .decorator import (
-    format,
-)
+from . import _utils
+from .decorator import format
 from .from_call_args import FromCallArgs
 from .partial import Partial
 from .types import Format, FormatT, Formattable, FormattingMode
@@ -19,5 +18,6 @@ __all__ = [
     "FormattingMode",
     "FromCallArgs",
     "Partial",
+    "_utils",
     "format",
 ]
---
+++ python/mirascope/llm/formatting/_utils.py
@@ -0,0 +1,25 @@
+"""Utils associated with formatting."""
+
+from typing import cast
+
+from .decorator import format
+from .types import (
+    Format,
+    FormatT,
+    Formattable,
+)
+
+
+def ensure_formattable(
+    formattable: type[FormatT],
+) -> Format:
+    """Ensures that a class is `Formattable`, and returns the format.
+
+    This is intended to be used by final consumers of `FormatT` (e.g. the clients) where
+    they need to ensure that the `FormatT` is `Formattable`, ie. that it has been decorated.
+
+    The `FormatT` will be decorated into a `Formattable` if it wasn't already.
+    """
+    if isinstance(formattable, Formattable):
+        return formattable.__response_format__
+    return cast(Formattable, format(formattable)).__response_format__
