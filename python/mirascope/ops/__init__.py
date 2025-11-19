581ad34ceed090fbade0c32a714ce247edf83399
Koudai Aono <koudai@mirascope.io>
2025-11-19 18:13:04 +0900

    Port context propagation from Lilypad into Mirascope v2

--- python/mirascope/ops/__init__.py
+++ python/mirascope/ops/__init__.py
@@ -14,6 +14,15 @@ from ._internal.session import (
 
 # TODO: refactor alongside all other import error handling
 with suppress(ImportError):
+    from ._internal.context import propagated_context
+    from ._internal.propagation import (
+        ContextPropagator,
+        PropagatorFormat,
+        extract_context,
+        get_propagator,
+        inject_context,
+        reset_propagator,
+    )
     from ._internal.spans import Span, span
     from ._internal.tracing import (
         AsyncTracedFunction,
@@ -23,18 +32,24 @@ with suppress(ImportError):
         TracedResult,
         trace,
     )
-
 __all__ = [
     "SESSION_HEADER_NAME",
     "AsyncTracedFunction",
     "AsyncTracedResult",
+    "ContextPropagator",
+    "PropagatorFormat",
     "SessionContext",
     "Span",
     "TraceDecorator",
     "TracedFunction",
     "TracedResult",
     "current_session",
+    "extract_context",
     "extract_session_id",
+    "get_propagator",
+    "inject_context",
+    "propagated_context",
+    "reset_propagator",
     "session",
     "span",
     "trace",
