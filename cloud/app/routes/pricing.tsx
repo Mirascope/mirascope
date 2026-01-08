81ce4162addf80f259ca09ddb4d64c4e5c12938f
Sebastian Huckleberry <sebastian@mirascope.com>
2026-01-07 16:07:52 -0800

    feat(cloud): add barebones pricing page

--- cloud/app/routes/pricing.tsx
+++ cloud/app/routes/pricing.tsx
@@ -1,7 +1,66 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { TempPage } from "@/app/components/temp-page";
+import { PricingPage } from "@/app/components/pricing-page";
+import { ButtonLink } from "@/app/components/ui/button-link";
+
+const marketingActions = {
+  hosted: {
+    free: {
+      button: (
+        <ButtonLink href="/docs" variant="default">
+          Get Started
+        </ButtonLink>
+      ),
+    },
+    pro: {
+      button: (
+        <ButtonLink href="mailto:sales@mirascope.com" variant="outline">
+          Contact Us
+        </ButtonLink>
+      ),
+    },
+    team: {
+      button: (
+        <ButtonLink href="mailto:sales@mirascope.com" variant="outline">
+          Contact Us
+        </ButtonLink>
+      ),
+    },
+  },
+  selfHosted: {
+    free: {
+      button: (
+        <ButtonLink href="/docs/getting-started/self-hosting" variant="default">
+          Get Started
+        </ButtonLink>
+      ),
+    },
+    pro: {
+      button: (
+        <ButtonLink href="mailto:sales@mirascope.com" variant="outline">
+          Request License
+        </ButtonLink>
+      ),
+    },
+    team: {
+      button: (
+        <ButtonLink href="mailto:sales@mirascope.com" variant="outline">
+          Request License
+        </ButtonLink>
+      ),
+    },
+  },
+};
 
 export const Route = createFileRoute("/pricing")({
-  // todo(sebastian): temp route ahead of porting md/mdx files
-  component: () => <TempPage name="Pricing" />,
+  // todo(sebastian): simplify and add other SEO metadata
+  head: () => ({
+    meta: [
+      { title: "Pricing" },
+      {
+        name: "description",
+        content: "Mirascope cloud's pricing plans and features",
+      },
+    ],
+  }),
+  component: () => <PricingPage actions={marketingActions} />,
 });
