73e7e01176f2f5853e9d0678c57d3c42002fede2
Sebastian Huckleberry <sebastian@mirascope.com>
2026-01-09 10:57:07 -0800

    chore(vite): tsconf path is no longer necessary in vite v7

--- cloud/vite.config.ts
+++ cloud/vite.config.ts
@@ -1,6 +1,5 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
-import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import path from "path";
@@ -16,9 +15,6 @@ export default defineConfig(() => {
       port: 3000,
     },
     plugins: [
-      tsConfigPaths({
-        projects: ["./tsconfig.json"],
-      }),
       viteContent({ contentDir }),
       viteMDX(),
       viteImages({ viteEnvironments: ["client"] }),
