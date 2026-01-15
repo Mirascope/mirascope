b1e5165a682962609a7b99f70a0b5d4a3880b165
William Bakst <william@mirascope.io>
2026-01-14 17:35:26 -0800

    feat(cloud): OrganizationInvitations schema

--- cloud/db/schema/index.ts
+++ cloud/db/schema/index.ts
@@ -3,6 +3,7 @@ export * from "@/db/schema/users";
 export * from "@/db/schema/organizations";
 export * from "@/db/schema/organization-memberships";
 export * from "@/db/schema/organization-membership-audit";
+export * from "@/db/schema/organization-invitations";
 export * from "@/db/schema/projects";
 export * from "@/db/schema/project-memberships";
 export * from "@/db/schema/project-membership-audit";
@@ -28,6 +29,10 @@ export type {
   PublicOrganizationMembershipAudit,
   AuditAction,
 } from "@/db/schema/organization-membership-audit";
+export type {
+  PublicOrganizationInvitation,
+  InvitationStatus,
+} from "@/db/schema/organization-invitations";
 export type { PublicProject } from "@/db/schema/projects";
 export type {
   PublicProjectMembership,
@@ -60,6 +65,7 @@ import { sessions } from "@/db/schema/sessions";
 import { organizations } from "@/db/schema/organizations";
 import { organizationMemberships } from "@/db/schema/organization-memberships";
 import { organizationMembershipAudit } from "@/db/schema/organization-membership-audit";
+import { organizationInvitations } from "@/db/schema/organization-invitations";
 import { projects } from "@/db/schema/projects";
 import { projectMemberships } from "@/db/schema/project-memberships";
 import { projectMembershipAudit } from "@/db/schema/project-membership-audit";
@@ -79,6 +85,7 @@ export type DatabaseTable =
   | typeof organizations
   | typeof organizationMemberships
   | typeof organizationMembershipAudit
+  | typeof organizationInvitations
   | typeof projects
   | typeof projectMemberships
   | typeof projectMembershipAudit
