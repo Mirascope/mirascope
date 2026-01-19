7f7e86c7cfc2f541258da332cccef2fca8c83786
William Bakst <william@mirascope.io>
2026-01-19 01:23:41 -0800

    chore(legal): Update website to enforce trademarks

--- cloud/app/components/blocks/navigation/footer.tsx
+++ cloud/app/components/blocks/navigation/footer.tsx
@@ -3,10 +3,13 @@ import { cn } from "@/app/lib/utils";
 
 export default function Footer() {
   return (
-    <footer className="mt-auto w-full px-4 pt-4 pb-3 sm:px-6 md:px-12">
+    <footer className="relative z-50 mt-auto w-full bg-background px-4 pt-4 pb-3 sm:px-6 md:px-12">
       <div className="mx-auto flex max-w-5xl flex-col-reverse items-center justify-between md:flex-row md:items-center">
         <div className="mt-4 text-center text-sm sm:text-base md:mt-0 md:text-left font-handwriting">
-          <p>© 2025 Mirascope. All rights reserved.</p>
+          <p>© {new Date().getFullYear()} Mirascope. All rights reserved.</p>
+          <p>
+            Mirascope® is a registered trademark of Mirascope, Inc. in the U.S.
+          </p>
         </div>
 
         <div className="flex gap-4 sm:gap-8">
